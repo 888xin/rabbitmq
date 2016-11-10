73f1a157836053d0b68e0cddf51282b330781ea6
henxinl <hengxinl@l99.com>
2016-11-10 18:36:04 +0800

    修改提交

---
+++ rabbit-demo/src/main/java/com/xin/simple/Reqv2.java
@@ -0,0 +1,35 @@
+package com.xin.simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * Created by lhx on 2016/9/5 11:04
+ *
+ * @Description 接收端
+ */
+public class Reqv2 {
+    private final static String QUEUE_NAME = "fanoutQueue4";
+
+    public static void main(String[] argv) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        QueueingConsumer consumer = new QueueingConsumer(channel);
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println(" [x] Received '" + message + "'");
+        }
+    }
+}
